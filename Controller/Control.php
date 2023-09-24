04dcaea621d085c9ce3bf94f21fab14a052629f7
Edduart <edduar13@gmail.com>
2023-09-24 09:33:23 -0400

    prueba

--- Controller/Control.php
+++ Controller/Control.php
@@ -19,6 +19,10 @@ class control{
 		include_once "View/Principal.php";
 	}
 
+	public function test(){
+		include_once "View/Principal.php";
+	}
+
 	public function sesion(){
 		include_once "View/login.php";
 	}
